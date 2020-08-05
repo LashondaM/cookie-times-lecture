99dc0c064ed377c81eabc9cb3bbcbf86ea378761
Lashonda Miller <lashondamiller007@gmail.com>
2020-08-05 16:04:57 -0400

    Updates

---
+++ precache-manifest.ad76edca27b171ee404292f1abceacf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7293731f68810aab920831fd5a0a305d",
+    "url": "/cookie-times-lecture/index.html"
+  },
+  {
+    "revision": "e6024425da995010e806",
+    "url": "/cookie-times-lecture/static/css/main.a70f5d9a.chunk.css"
+  },
+  {
+    "revision": "2a4127141215e2cfbd95",
+    "url": "/cookie-times-lecture/static/js/2.ff157c78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cookie-times-lecture/static/js/2.ff157c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6024425da995010e806",
+    "url": "/cookie-times-lecture/static/js/main.b83e888a.chunk.js"
+  },
+  {
+    "revision": "2564dbaefa6e60a8b7ad",
+    "url": "/cookie-times-lecture/static/js/runtime-main.87e52042.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/cookie-times-lecture/static/media/pokeball.f10bf078.png"
+  }
+]);
